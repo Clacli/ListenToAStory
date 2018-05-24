d5ec9e047f6099b461efafd7ce79a29ce30f28dc
ClaudiaBee <claudiabellan.herb@gmail.com>
2018-05-24 21:58:44 +0200

    Send an Send an explicit intent to start NowPlayingActivity

--- app/src/main/java/com/example/claudiabee/listentoastory/LibraryActivity.java
+++ app/src/main/java/com/example/claudiabee/listentoastory/LibraryActivity.java
@@ -1,7 +1,10 @@
 package com.example.claudiabee.listentoastory;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class LibraryActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class LibraryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_library);
+
+        TextView toNowPlayingActivity = findViewById(R.id.to_now_playing_activity);
+        toNowPlayingActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), NowPlayingActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
