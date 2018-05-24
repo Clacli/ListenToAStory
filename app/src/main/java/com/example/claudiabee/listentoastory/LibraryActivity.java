139e70dbf1dcedcb799c20cef2c9097aa38eacb3
ClaudiaBee <claudiabellan.herb@gmail.com>
2018-05-24 21:35:09 +0200

    Add Library activity

---
+++ app/src/main/java/com/example/claudiabee/listentoastory/LibraryActivity.java
@@ -0,0 +1,13 @@
+package com.example.claudiabee.listentoastory;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class LibraryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_library);
+    }
+}
