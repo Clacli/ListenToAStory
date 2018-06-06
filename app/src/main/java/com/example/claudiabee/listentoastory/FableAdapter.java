dfd044fc5e247ee24c9fd4262d4f542fcd583e28
ClaudiaBee <claudiabellan.herb@gmail.com>
2018-06-06 15:28:11 +0200

    Improve efficiency of adapter with a ViewHolder

--- app/src/main/java/com/example/claudiabee/listentoastory/FableAdapter.java
+++ app/src/main/java/com/example/claudiabee/listentoastory/FableAdapter.java
@@ -17,10 +17,55 @@ import java.util.ArrayList;
  */
 public class FableAdapter extends ArrayAdapter<Fable> {
 
-    private TextView fableTitleTextView;
-    private TextView readerNameTextView;
-    private TextView bookTitleTextView;
-    private TextView fableDurationTextView;
+    FableViewHolder holder;
+
+    /**
+     * Provides a view for an adapterView(ListView, it could be a GridView)
+     */
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        // Check if the existing view is being reused, otherwise inflate the view
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.fable_list_item, parent, false);
+
+            // Set the view ViewHolder
+            holder = new FableViewHolder();
+            holder.fableTitleTextView = convertView.findViewById(R.id.fable_title);
+            holder.readerNameTextView = convertView.findViewById(R.id.reader_name);
+            holder.bookTitleTextView = convertView.findViewById(R.id.book_title);
+            holder.fableDurationTextView = convertView.findViewById(R.id.duration);
+            convertView.setTag(holder);
+        } else {
+            // we've just avoided calling findViewById() on resource everytime
+            // just use the viewHolder
+            holder = (FableViewHolder) convertView.getTag();
+        }
+
+        // Get the {@link Fable} object located at this position in the list
+        Fable currentFable = getItem(position);
+
+        // Get the title of the fable from the currentFable object and set this text
+        //on the fableTitleTextView
+        holder.fableTitleTextView.setText(currentFable.getFableTitle());
+
+        // Get the reader's name from the currentFable object and set this text
+        // in the readerNameTextView
+        holder.readerNameTextView.setText(currentFable.getReaderName());
+
+        // Get the title of the book from the current Fable object and set this text
+        // in the bookTitletextView
+        holder.bookTitleTextView.setText(currentFable.getBookTitle());
+
+        // Get the duration of the fable audio file from the current Fable object and set this text
+        // in the fableDurationTextView
+        holder.fableDurationTextView.setText(currentFable.getAudioDuration());
+
+        // Return the whole list item layout (containing an ImageView and four TextView)
+        // so that it can be shown in the ListView
+        return convertView;
+    }
 
     /**
      * Create a new {@link FableAdapter} object.
@@ -32,50 +77,11 @@ public class FableAdapter extends ArrayAdapter<Fable> {
         super(context, 0, fables);
     }
 
-    /**
-     * Provides a view for an adapterView(ListView, it could be a GridView)
-     */
-    @NonNull
-    @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        // Check if the existing view is being reused, otherwise inflate the view
-        if (convertView == null) {
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.fable_list_item, parent, false);
-        }
-
-
-        // Get the {@link Fable} object located at this position in the list
-        Fable currentFable = getItem(position);
-
-        //Find the TextView in the fable_list_item.xml layout with ID fable_title
-        fableTitleTextView = convertView.findViewById(R.id.fable_title);
-
-        // Get the title of the fable from the currentFable object and set this text
-        //on the fableTitleTextView
-        fableTitleTextView.setText(currentFable.getFableTitle());
-
-        // Find the TextView in the fable_list_item.xml layout with ID reader_name
-        readerNameTextView = convertView.findViewById(R.id.reader_name);
-
-        // Get the reader's name from the currentFable object and set this text
-        // in the readerNameTextView
-        readerNameTextView.setText(currentFable.getReaderName());
-
-        // Find the TextView in the fable_list_item.xml layout with ID book_title
-        bookTitleTextView = convertView.findViewById(R.id.book_title);
-        // Get the title of the book from the current Fable object and set this text
-        // in the bookTitletextView
-        bookTitleTextView.setText(currentFable.getBookTitle());
-
-        // Find the textView in the fable_list_item.xml layout with ID duration
-        fableDurationTextView = convertView.findViewById(R.id.duration);
-
-        // Get the duration of the fable audio file from the current Fable object and set this text
-        // in the fableDurationTextView
-        fableDurationTextView.setText(currentFable.getAudioDuration());
-
-        // Return the whole list item layout (containing an ImageView and four TextView)
-        // so that it can be shown in the ListView
-        return convertView;
+    //
+    static class FableViewHolder {
+        TextView fableTitleTextView;
+        TextView readerNameTextView;
+        TextView bookTitleTextView;
+        TextView fableDurationTextView;
     }
 }
